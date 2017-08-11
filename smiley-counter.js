e9e3e189dcddef3b8ed72d65e3b8c8da6cf83849
Isaac Vallee <isaacvallee@gmail.com>
2017-08-11 23:08:58 +0000

    adds smiley counter challenge

---
+++ smiley-counter.js
@@ -0,0 +1,13 @@
+//return the total number of smiling faces in the array
+function countSmileys(arr) {
+  return arr.filter(x => /^[:;][-~]?[)D]$/.test(x)).length;
+}
+
+
+console.log(countSmileys([]), 0);
+console.log(countSmileys([':D',':~)',';~D',':)']), 4);
+console.log(countSmileys([':)',':(',':D',':O',':;']), 2);
+console.log(countSmileys([';]', ':[', ';*', ':$', ';-D']), 1);
+
+// I wasn't able to figure out this challenge this time!
+// The function above is one of the solutions using regular expressions
